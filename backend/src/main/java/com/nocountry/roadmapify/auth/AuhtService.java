38706af8ad4e7dcfe68cbca4df79e9937e74d49c
sturniolo <br1.sturniolo@gmail.com>
2023-10-19 14:48:50 -0300

    [ADD]logueo con spring security

---
+++ backend/src/main/java/com/nocountry/roadmapify/auth/AuhtService.java
@@ -0,0 +1,48 @@
+package com.nocountry.roadmapify.auth;
+
+import com.nocountry.roadmapify.jwt.JwtService;
+import com.nocountry.roadmapify.user.Role;
+import com.nocountry.roadmapify.user.User;
+import com.nocountry.roadmapify.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuhtService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse
+                .builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .experience(0.0)
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
+    }
+}
