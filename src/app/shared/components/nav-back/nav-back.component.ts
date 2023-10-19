8ae787663a2ef17b31d6353d4a75fe0782d6f2ec
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-10-19 19:23:45 -0300

    feat(frontend): add components search and nav-back

---
+++ src/app/shared/components/nav-back/nav-back.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-back',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-back.component.html',
+  styleUrls: ['./nav-back.component.scss']
+})
+export class NavBackComponent {
+
+}
