4a9401d885b7f6a1a78ae8b520705d6bc52cb817
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-10-19 19:20:31 -0300

    feat(frontend): add card role

---
+++ src/app/shared/components/card-role/card-role.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {IconRoleComponent} from "../icon-role/icon-role.component";
+import {Color} from "../../../core/ui/types/color.type";
+
+@Component({
+  selector: 'app-card-role',
+  standalone: true,
+  imports: [CommonModule, IconRoleComponent],
+  templateUrl: './card-role.component.html',
+  styleUrls: ['./card-role.component.scss']
+})
+export class CardRoleComponent {
+  @Input() title!: string;
+  @Input() showTitle: boolean = true;
+  @Input() borderColor: Color = "black";
+  @Input() backgroundColor: Color = "white";
+
+
+}
