061f9bb49f2580f344ad9faddeef932b0f2f14df
danielAlderete93 <daniel.a.alderete93@gmail.com>
2023-10-30 13:28:06 -0300

    feat(frontend): add roadmap page

---
+++ src/app/roadmap/pages/roadmap-page/roadmap.page.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+
+@Component({
+  selector: 'app-roadmap-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './roadmap.page.html',
+  styleUrls: ['./roadmap.page.scss']
+})
+export class RoadmapPage {
+
+  constructor() {
+  }
+
+
+}
+
+
